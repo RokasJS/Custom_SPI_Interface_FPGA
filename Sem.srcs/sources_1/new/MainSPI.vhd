17f7a96232fde6ce6d74f1795429b88f6e26a9b9
Rokas <rokas325172@gmail.com>
2020-12-10 17:55:22 +0200

    Working transmitting of data with divided clock. Basys 3 Board

--- Sem.srcs/sources_1/new/MainSPI.vhd
+++ Sem.srcs/sources_1/new/MainSPI.vhd
@@ -7,18 +7,25 @@ use ieee.std_logic_arith.all;
 entity MainSPI is
 port (
     clk : in STD_LOGIC;
+    segment: out STD_LOGIC_VECTOR (6 downto 0);
+    led : out STD_LOGIC_VECTOR (15 downto 0);
+    JA : out std_logic_vector (7 downto 0); 
     sw : in STD_LOGIC_VECTOR (3 downto 0);
     btnL : in std_logic;
     reset_n : in std_logic;
-    btnD : in std_logic;
-    spi_bit : inout std_logic;
-    spi_clock : inout std_logic;
-    spi_bit_RX : inout std_logic;
-    RX_Data : inout std_logic_vector(7 downto 0)
+    btnD : in std_logic
+    
+--    
+--    spi_bit_RX : inout std_logic;
+--    RX_Data : inout std_logic_vector(7 downto 0)
     );
 end MainSPI;
 
 architecture Behavioral of MainSPI is
+    constant Period: integer := 50000; 
+    constant Period_2: integer := 50000; 
+    signal delay :STD_LOGIC_VECTOR (31 downto 0) :=X"00000000";
+    signal spi_bit : std_logic;
     signal packet_1: std_logic_vector(7 downto 0);
     signal packet_2: std_logic_vector(7 downto 0);
     signal counter : integer := 0;
@@ -27,6 +34,7 @@ architecture Behavioral of MainSPI is
     signal check : std_logic := '1';
     signal RX_Dataa : std_logic_vector(7 downto 0) := "00000000";
     signal RX_Counter : integer := 0;
+    signal spi_clock : std_logic := '1';
 begin
     packet_1(3 downto 0) <= "0001";
     packet_1(7 downto 4) <= sw(3 downto 0);
@@ -36,13 +44,18 @@ begin
 process (clk)
 begin
     if falling_edge(clk) then
+    if delay=CONV_STD_LOGIC_VECTOR(Period,32) then
+        
+        delay<=X"00000000";
         if btnD = '1' then
+            spi_clock <= not spi_clock;
             spi_bit <= packet_1(counter);
             case counter is 
                 when 7 => counter <= 0;
                 when others => counter <= counter+1;
             end case; 
         elsif (reset_n = '1' and btnL = '1') then
+            spi_clock <= not spi_clock;
             spi_bit <= packet_2(counter);
             case counter is 
                 when 7 => counter <= 0;
@@ -52,31 +65,34 @@ begin
             spi_bit <= '0';
             spi_clock <= '1';
             counter <= 0;
-        end if;   
-    end if;
-    if (btnD = '1' or (btnL = '1' and reset_n = '1')) then
-        spi_clock <= clk;
+        end if;
+    else 
+        delay<=delay+1;
+    end if;   
     end if;
 end process;
 
-process(clk,spi_clock) 
-begin
-    if rising_edge(clk) then
-        if (btnD = '0' and btnL = '0' and reset_n = '0') then
-            counter_RX <= 0;
-            RX_Dataa(7 downto 0) <= "00000000";
-        end if;
-    end if;
-    if rising_edge(spi_clock) then
-        if (btnD = '1' or (btnL = '1' and reset_n = '1')) then
-        RX_Dataa(counter_RX) <= spi_bit_RX;
-        case counter_RX is 
-            when 7 => counter_RX <= 0;
-            when others => counter_RX <= counter_RX+1;
-        end case;
-        end if;
-    end if;
-end process;
-spi_bit_RX <= spi_bit;
-RX_Data(7 downto 0) <= RX_Dataa(7 downto 0);
+--process(clk,spi_clock) 
+--begin
+--    if rising_edge(clk) then
+--        if (btnD = '0' and btnL = '0' and reset_n = '0') then
+--            counter_RX <= 0;
+--            RX_Dataa(7 downto 0) <= "00000000";
+--        end if;
+--    end if;
+--    if rising_edge(spi_clock) then
+--        if (btnD = '1' or (btnL = '1' and reset_n = '1')) then
+--        RX_Dataa(counter_RX) <= spi_bit_RX;
+--        case counter_RX is 
+--            when 7 => counter_RX <= 0;
+--            when others => counter_RX <= counter_RX+1;
+--        end case;
+--        end if;
+--    end if;
+--end process;
+JA(0) <= spi_bit;
+JA(1) <= spi_clock;
+JA(2) <= clk;
+--spi_bit_RX <= spi_bit;
+--RX_Data(7 downto 0) <= RX_Dataa(7 downto 0);
 end Behavioral;
